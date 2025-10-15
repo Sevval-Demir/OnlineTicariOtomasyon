45e18cd5abf4985634804e076691625fe9c1eaea
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-10-15 21:54:54 +0300

    departman section completed

---
+++ sqlsources/SQLQuery3.sql
@@ -0,0 +1,8 @@
+insert into dbo.Carilers
+(CariAd,CariSoyad,CariSehir,CariMail)
+values 
+('Ayþe','Kaya','Ankara','deneme'),
+('Salih','Beyaz','Trabzon','deneme'),
+('Barýþ','San','Ankara','Deneme');
+
+select * from Carilers;
