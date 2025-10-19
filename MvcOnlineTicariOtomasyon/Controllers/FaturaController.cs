f76abe021bed777d3b36b5eeb84fa6b1d862d6a7
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-10-19 14:55:50 +0300

    faturalar started

---
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+    }
+}
