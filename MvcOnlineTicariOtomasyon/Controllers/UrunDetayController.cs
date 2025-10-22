ede805acdb2ce4372deb129899500c20f649ea35
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-10-22 20:47:41 +0300

    galeri section completed

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        // GET: UrunDetay
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
