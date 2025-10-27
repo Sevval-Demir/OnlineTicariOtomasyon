65d23cbec2fa4f6a8ebb9ff5daec074b2b1305af
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-10-27 20:56:56 +0300

    simple tables started

--- MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -14,8 +14,8 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         {
             Class1 cs = new Class1();
             //var degerler = c.Uruns.Where(x => x.UrunID == 1).ToList();
-            cs.Deger1=c.Uruns.Where(x=>x.UrunID==1).ToList();
-            cs.Deger2=c.Detays.Where(y=>y.DetayID==1).ToList();
+            cs.Deger1=c.Uruns.Where(x=>x.UrunID==11).ToList();
+            cs.Deger2=c.Detays.Where(y=>y.DetayID==11).ToList();
             return View(cs);
         }
     }
