c2b496bd50db523abbc43c0bc111eac643dacad2
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-10-29 14:15:34 +0300

    partial view completed

--- MvcOnlineTicariOtomasyon/Controllers/IstatistikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/IstatistikController.cs
@@ -79,13 +79,34 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         public PartialViewResult Partial1()
         {
             var sorgu2 = from x in c.Personels
-                         group x by x.Departmanid into g
+                         group x by x.Departman.DepartmanAd into g
                          select new SinifGrup2
                          {
                              Departman = g.Key,
                              Sayi = g.Count()
                          };
             return PartialView(sorgu2.ToList());
+        }
+        public PartialViewResult Partial2()
+        {
+            var sorgu = c.Carilers.ToList();
+            return PartialView(sorgu);
+        }
+        public PartialViewResult Partial3()
+        {
+            var sorgu = c.Uruns.ToList();
+            return PartialView(sorgu);
+        }
+        public PartialViewResult Partial4()
+        {
+            var sorgu = from x in c.Uruns
+                         group x by x.Marka into g
+                         select new SinifGrup3
+                         {
+                             Marka = g.Key,
+                             sayi = g.Count()
+                         };
+            return PartialView(sorgu.ToList());
         }
     }
 }
