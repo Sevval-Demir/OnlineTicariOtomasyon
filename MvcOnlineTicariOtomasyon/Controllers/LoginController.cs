e7217e98fc61f53df49f603afca28e93078fd2cc
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-12-08 20:46:43 +0300

    login started

---
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Mvc;
+using Context = MvcOnlineTicariOtomasyon.Models.Sınıflar.Context;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return PartialView();
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Personel.cs
@@ -13,12 +13,15 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
         public int PersonelID { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Display(Name ="Personel Adı")]
         public string PersonelAd { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Display(Name = "Personel Soyadı")]
         public string PersonelSoyad { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
+        [Display(Name = "Personel Görseli")]
         public string PersonelGorsel { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
         public int Departmanid { get; set; }
