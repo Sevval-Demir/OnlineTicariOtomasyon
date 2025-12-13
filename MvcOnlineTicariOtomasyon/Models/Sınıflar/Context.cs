982be254176230adebf624cd041cb5901ca1670c
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-12-13 16:21:29 +0300

    kargo section has started

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Context.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Context.cs
@@ -19,5 +19,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
         public DbSet<Urun> Uruns { get; set; }
         public DbSet<Detay> Detays { get; set; }
         public DbSet<Yapilacak> Yapilacaks { get; set; }
+        public DbSet<KargoDetay> KargoDetays { get; set; }
+        public DbSet<KargoTakip> kargoTakips { get; set; }
     }
 }
