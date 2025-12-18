a139b3ea1a371a5410f4c43e6e4d194f56878790
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-12-19 01:18:21 +0300

    mesajlar section started

--- MvcOnlineTicariOtomasyon/Models/Sınıflar/Context.cs
+++ MvcOnlineTicariOtomasyon/Models/Sınıflar/Context.cs
@@ -21,5 +21,6 @@ namespace MvcOnlineTicariOtomasyon.Models.Sınıflar
         public DbSet<Yapilacak> Yapilacaks { get; set; }
         public DbSet<KargoDetay> KargoDetays { get; set; }
         public DbSet<KargoTakip> kargoTakips { get; set; }
+        public DbSet<mesajlar> mesajlars { get; set; }
     }
 }
---
+++ sqlsources/SQLQuery6.sql
@@ -0,0 +1,2 @@
+Select CariAd + ' ' + CariSoyad From Carilers Where
+CariMail in(Select Gonderici From mesajlars)
